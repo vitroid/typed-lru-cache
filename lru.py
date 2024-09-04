1f6005b630e9e01030ff014ee63ab3038ccaed18
vitroid <vitroid@gmail.com>
2024-09-05 02:49:18 +0900

    implemented shelve_cache and sqlitedict_cache

--- lru.py
+++ lru.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 from collections.abc import Callable
 from typing import Final, Generic, Hashable, NamedTuple, Optional, ParamSpec, TypeVar
+from logging import getLogger
 
 T = TypeVar("T")
 P = ParamSpec("P")
@@ -81,6 +82,102 @@ class _LruCacheFunctionWrapper(Generic[P, T]):
 def lru_cache(maxsize: int) -> Callable[[Callable[P, T]], _LruCacheFunctionWrapper[P, T]]:
     def decorator(func: Callable[P, T]) -> _LruCacheFunctionWrapper[P, T]:
         wrapped = _LruCacheFunctionWrapper(func, maxsize)
+        wrapped.__doc__ = func.__doc__
+        return wrapped
+
+    return decorator
+
+
+# LRUのないただの無制限キャッシュ。これをディスク上に実装すればいい。
+class _CacheFunctionWrapper(Generic[P, T]):
+    def __init__(self, func: Callable[P, T]):
+        self.__wrapped__ = func
+        self.__cache = dict()
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> T:
+
+        call_args = args + tuple(kwargs.items())
+
+        if call_args not in self.__cache:
+            ret = self.__wrapped__(*args, **kwargs)
+            self.__cache[call_args] = ret
+        else:
+            ret = self.__cache[call_args]
+        return ret
+
+    @property
+    def cache(self) -> dict:
+        return self.__cache
+
+
+def cache() -> Callable[[Callable[P, T]], _CacheFunctionWrapper[P, T]]:
+    def decorator(func: Callable[P, T]) -> _CacheFunctionWrapper[P, T]:
+        wrapped = _CacheFunctionWrapper(func)
+        wrapped.__doc__ = func.__doc__
+        return wrapped
+
+    return decorator
+
+
+# shelves cache
+import shelve
+import json
+
+
+class _ShelfCacheFunctionWrapper(Generic[P, T]):
+    def __init__(self, func: Callable[P, T], basename: str):
+        self.__wrapped__ = func
+        self.__basename = basename
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> T:
+        logger = getLogger()
+        call_args = json.dumps(args + tuple(kwargs.items()))
+        logger.debug(call_args)
+        with shelve.open(f"{self.__basename}.shelf") as shelf:
+            if call_args not in shelf:
+                ret = self.__wrapped__(*args, **kwargs)
+                shelf[call_args] = ret
+            else:
+                ret = shelf[call_args]
+        return ret
+
+
+def shelf_cache(basename: str) -> Callable[[Callable[P, T]], _ShelfCacheFunctionWrapper[P, T]]:
+    def decorator(func: Callable[P, T]) -> _ShelfCacheFunctionWrapper[P, T]:
+        wrapped = _ShelfCacheFunctionWrapper(func, basename)
+        wrapped.__doc__ = func.__doc__
+        return wrapped
+
+    return decorator
+
+
+# sqlitedict cache
+import sqlitedict
+import json
+
+
+class _SQLiteDictCacheFunctionWrapper(Generic[P, T]):
+    def __init__(self, func: Callable[P, T], basename: str):
+        self.__wrapped__ = func
+        self.__basename = basename
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> T:
+        logger = getLogger()
+        call_args = json.dumps(args + tuple(kwargs.items()))
+        logger.debug(call_args)
+        with sqlitedict.open(f"{self.__basename}.sqlite") as shelf:
+            if call_args not in shelf:
+                ret = self.__wrapped__(*args, **kwargs)
+                shelf[call_args] = ret
+                shelf.commit()
+            else:
+                ret = shelf[call_args]
+        return ret
+
+
+def sqlitedict_cache(basename: str) -> Callable[[Callable[P, T]], _SQLiteDictCacheFunctionWrapper[P, T]]:
+    def decorator(func: Callable[P, T]) -> _SQLiteDictCacheFunctionWrapper[P, T]:
+        wrapped = _SQLiteDictCacheFunctionWrapper(func, basename)
         wrapped.__doc__ = func.__doc__
         return wrapped
 
